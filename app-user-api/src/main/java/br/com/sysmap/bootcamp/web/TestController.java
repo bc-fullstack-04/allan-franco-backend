e33dd4c50af5ab4e59343701189c68e1acf0c71f
Allan Franco <allan.work19@gmail.com>
2024-04-15 23:22:06 -0300

    Configure environment & Hello World

---
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/web/TestController.java
@@ -0,0 +1,15 @@
+package br.com.sysmap.bootcamp.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
---
+++ app-user-api/src/test/java/br/com/sysmap/bootcamp/BootcampSysmapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSysmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
