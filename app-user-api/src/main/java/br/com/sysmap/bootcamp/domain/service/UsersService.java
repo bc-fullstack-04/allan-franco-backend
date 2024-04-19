ed47a68f928f7ec1b02c517b6a62bdbf930d3c1c
Allan Franco <allan.work19@gmail.com>
2024-04-18 21:21:35 -0300

    Implementation of method updateByFields of Users

--- app-user-api/src/main/java/br/com/sysmap/bootcamp/domain/service/UsersService.java
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/domain/service/UsersService.java
@@ -7,8 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,14 +21,29 @@ public class UsersService {
 
     private final UsersRepository usersRepository;
 
-    //POST
     @Transactional(propagation = Propagation.REQUIRED)
+
+    //POST
     public Users save(Users user){
         log.info("Saving user: {}", user);
         return this.usersRepository.save(user);
     }
 
     //PUT
+    public Users updateByFields(long id, Map<String, Object> fields) {
+        Optional<Users> findedUser = usersRepository.findById(id);
+
+        if (findedUser.isPresent()) {
+            fields.forEach((key, value) -> {
+                Field field = ReflectionUtils.findField(Users.class, key);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, findedUser.get(), value);
+            });
+            log.info("Updating user...");
+            return usersRepository.save(findedUser.get());
+        }
+        return null;
+    }
 
 
     //GET
--- app-user-api/src/main/java/br/com/sysmap/bootcamp/web/UsersController.java
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/web/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,22 +17,25 @@ public class UsersController {
     private final UsersService usersService;
 
     //POST
-    @PostMapping("/save")
+    @PostMapping("save")
     public ResponseEntity<Users> save(@RequestBody Users user){
         return ResponseEntity.ok(this.usersService.save(user));
     }
 
     //PUT
-
+    @PutMapping("update/{id}")
+    public ResponseEntity<Users> updateByFields(@PathVariable String id,@RequestBody Map<String, Object> fields){
+        return ResponseEntity.ok(this.usersService.updateByFields(Long.parseLong(id), fields));
+    }
 
     //GET SPECIFIC USER
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Users> getOneUser(@PathVariable String id){
         return ResponseEntity.ok(this.usersService.getOneUser(Long.parseLong(id)));
     }
 
     //GET ALL USERS
-    @GetMapping("/all")
+    @GetMapping()
     public ResponseEntity<List<Users>> getAllUser(){
         return ResponseEntity.ok(this.usersService.getAllUser());
     }
